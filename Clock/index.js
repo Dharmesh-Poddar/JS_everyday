a3c17763c0103d78dc51d33c9afd60d3682aebb9
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-07-02 22:28:52 +0530

    hourhand

--- Clock/index.js
+++ Clock/index.js
@@ -8,7 +8,11 @@ function setDate(){
     const seconds=now.getSeconds();
     const secondDegrees =((seconds/60)*360)+90;
     secondHand.getElementsByClassName.transform= 'rotate(${secondsDegrees}deg)';
+    
 
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 }
 setInterval(setDate, 1000);
 setDate();
