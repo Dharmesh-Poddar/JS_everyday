6c2c5874b31bdaaca2d7005c50821f675d3892a7
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-07-19 20:03:05 +0530

    rps

--- rockPaperScissor game/main.js
+++ rockPaperScissor game/main.js
@@ -6,10 +6,33 @@ const scoreBoard_div =document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div =document.getElementById("r");
 const paper_div =document.getElementById("p");
-const scissrs_div= document.getElementById("s");
+const scissors_div= document.getElementById("s");
 
+
+function main(){
+
+    function game(userInput){
+     
+         console.log('you clicked  '+ userInput );
+
+
+    }
 
 rock_div.addEventListener('click',function(){
-    console.log('you clicked rock');
-    
+    game("r");
+
+
 })
+
+paper_div.addEventListener('click',function(){
+   game("p");
+
+
+})
+scissors_div.addEventListener('click',function(){
+ game("s");
+
+})
+}
+
+main();
